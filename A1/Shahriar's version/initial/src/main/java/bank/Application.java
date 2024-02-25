9d078c265386aeb97afd91cbb355e732f07d5179
Shahriar-0 <attarshahriar@gmail.com>
2024-02-25 10:18:52 +0330

    fix small things

--- A1/Shahriar's version/initial/src/main/java/bank/Application.java
+++ A1/Shahriar's version/initial/src/main/java/bank/Application.java
@@ -2,7 +2,6 @@ package bank;
 
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -15,9 +14,6 @@ public class Application {
 	@Autowired
 	private JmsTemplate jmsTemplate;
 
-	// public static void main(String[] args) {
-	// 	SpringApplication.run(Application.class, args);
-	// }
 
 	@JmsListener(destination = INQ)
 	public void listener(String message) {
