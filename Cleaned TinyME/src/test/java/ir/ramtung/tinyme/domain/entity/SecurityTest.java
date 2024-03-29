36410f6d5f58a68798becf66b9c55262f3454fad
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-03-29 08:07:25 +0330

    add add_buy_ice_order_and_partially_traded_and_remainder_is_bigger_than_peak_size

--- Cleaned TinyME/src/test/java/ir/ramtung/tinyme/domain/entity/SecurityTest.java
+++ Cleaned TinyME/src/test/java/ir/ramtung/tinyme/domain/entity/SecurityTest.java
@@ -826,4 +826,18 @@ public class SecurityTest {
         AssertingPack.assertOrderInQueue(Side.BUY, 0, 6, 3, 600);
         AssertingPack.assertOrderInQueue(Side.SELL, 0, 2, 10, 700);
     }
+
+    @Test
+    public void add_buy_ice_order_and_partially_traded_and_remainder_is_bigger_than_peak_size() {
+        IcebergOrder order = new IcebergOrder(6, security, Side.BUY, 13, 600, buyerBroker, buyerShareholder, 2);
+        buyerBroker.increaseCreditBy(7800);
+        security.addNewOrder(order, matcher);
+
+        AssertingPack.exceptedBuyerPosition = 10;
+        AssertingPack.exceptedSellerCredit = 6000;
+        AssertingPack.exceptedSellerPosition = 75;
+        AssertingPack.assertAll();
+        AssertingPack.assertOrderInQueue(Side.BUY, 0, 6, 3, 600, 2, 2);
+        AssertingPack.assertOrderInQueue(Side.SELL, 0, 2, 10, 700);
+    }
 }
