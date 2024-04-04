f25ad84a4a61263d94390915f44bc81f8ce0297f
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-04-04 19:17:03 +0330

    add notEnoughExecution to MatchingOutcome

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/entity/MatchResult.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/entity/MatchResult.java
@@ -21,6 +21,10 @@ public final class MatchResult {
         return new MatchResult(MatchingOutcome.NOT_ENOUGH_POSITIONS, null, new LinkedList<>());
     }
 
+    public static MatchResult notEnoughExecution() {
+        return new MatchResult(MatchingOutcome.NOT_ENOUGH_EXECUTION, null, new LinkedList<>());
+    }
+
     private MatchResult(MatchingOutcome outcome, Order remainder, LinkedList<Trade> trades) {
         this.outcome = outcome;
         this.remainder = remainder;
