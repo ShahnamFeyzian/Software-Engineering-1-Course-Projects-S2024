4997c20a1b07ac515b20da67d847e79cfd60bbae
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-04-05 18:34:41 +0330

    add add_sell_order_matches_with_all_buyer_queue_and_not_finished

--- TinyME_v2.0/src/test/java/ir/ramtung/tinyme/domain/entity/SecurityTest.java
+++ TinyME_v2.0/src/test/java/ir/ramtung/tinyme/domain/entity/SecurityTest.java
@@ -405,6 +405,12 @@ public class SecurityTest {
             return security.addNewOrder(order, matcher);
         }
 
+        public MatchResult add_sell_order_matches_with_all_buyer_queue_and_not_finished() {
+            Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
+            sellerShareholder.incPosition(security, 120);
+            return security.addNewOrder(order, matcher);
+        }
+
         // TODO
     }
 
@@ -2348,52 +2354,48 @@ public class SecurityTest {
     }
 
     @Test
-    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_buyer_credit() {
-        Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
-        sellerShareholder.incPosition(security, 120);
-        security.addNewOrder(order, matcher);
-
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_match_result() {
+        MatchResult res = scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
+        assertThat(res.outcome()).isEqualTo(MatchingOutcome.EXECUTED);
+    }
+    
+    @Test
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_buyer_credit() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
         assertPack.assertBuyerCredit();
     }
 
     @Test
-    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_buyer_position() {
-        Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
-        sellerShareholder.incPosition(security, 120);
-        security.addNewOrder(order, matcher);
-
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_buyer_position() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
         assertPack.exceptedBuyerPosition = 85;
         assertPack.assertBuyerPosition();
     }
 
     @Test
-    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_seller_credit() {
-        Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
-        sellerShareholder.incPosition(security, 120);
-        security.addNewOrder(order, matcher);
-
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_seller_credit() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
         assertPack.exceptedSellerCredit = 32500;
         assertPack.assertSellerCredit();
     }
 
     @Test
-    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_seller_position() {
-        Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
-        sellerShareholder.incPosition(security, 120);
-        security.addNewOrder(order, matcher);
-
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_seller_position() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
         assertPack.exceptedSellerPosition = 120;
         assertPack.assertSellerPosition();
     }
 
     @Test
-    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_order_in_queue() {
-        Order order = new Order(6, security, Side.SELL, 120, 100, sellerBroker, sellerShareholder);
-        sellerShareholder.incPosition(security, 120);
-        security.addNewOrder(order, matcher);
-
-        assertThat(orderBook.getBuyQueue().size()).isZero();
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_sell_side_in_queue() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
         assertPack.assertOrderInQueue(Side.SELL, 0, 6, 35, 100);
+    }
+
+    @Test
+    public void add_sell_order_matches_with_all_buyer_queue_and_not_finished_and_check_buy_side_in_queue() {
+        scenarioGenerator.add_sell_order_matches_with_all_buyer_queue_and_not_finished();
+        assertThat(orderBook.getBuyQueue().size()).isZero();
     }
 
     @Test
