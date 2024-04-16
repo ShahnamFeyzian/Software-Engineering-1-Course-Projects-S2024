38374f63ee870375d6318ac409fcf4a8de712d20
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-04-16 13:44:03 +0330

    override queuesBefore

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrder.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrder.java
@@ -1,10 +1,26 @@
 package ir.ramtung.tinyme.domain.entity;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class StopLimitOrder extends Order {
     private int stopPrice;
 
     public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, int stopPrice) {
         super(orderId, security, side, quantity, price, broker, shareholder);
         this.stopPrice = stopPrice;
+    }
+
+    @Override 
+    public boolean queuesBefore(Order order) {
+        StopLimitOrder sloOrder = (StopLimitOrder) order;
+        if (this.side == Side.BUY)
+            return stopPrice < sloOrder.getStopPrice();
+        else
+            return stopPrice > sloOrder.getStopPrice();
     }
 }
