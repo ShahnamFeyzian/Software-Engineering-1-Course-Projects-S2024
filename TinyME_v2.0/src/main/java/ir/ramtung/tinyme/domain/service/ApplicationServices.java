ef5ad3e3258df29696e41f9139390fae95edd94b
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-04-27 16:19:24 +0330

    move validation from OrderHandler to ApplicationServices

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/ApplicationServices.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/ApplicationServices.java
@@ -4,38 +4,114 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-import ir.ramtung.tinyme.domain.entity.MatchResult;
+import ir.ramtung.tinyme.domain.entity.*;
+import ir.ramtung.tinyme.domain.exception.InvalidIcebergPeakSizeException;
+import ir.ramtung.tinyme.domain.exception.InvalidPeakSizeException;
+import ir.ramtung.tinyme.domain.exception.InvalidStopLimitPriceException;
+import ir.ramtung.tinyme.domain.exception.NotFoundException;
+import ir.ramtung.tinyme.domain.exception.UpdateMinimumExecutionQuantityException;
+import ir.ramtung.tinyme.messaging.Message;
+import ir.ramtung.tinyme.messaging.exception.InvalidRequestException;
 import ir.ramtung.tinyme.messaging.request.DeleteOrderRq;
 import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
+import ir.ramtung.tinyme.repository.BrokerRepository;
+import ir.ramtung.tinyme.repository.SecurityRepository;
+import ir.ramtung.tinyme.repository.ShareholderRepository;
 
 @Service
 public class ApplicationServices {
-    
+    private SecurityRepository securityRepository;
+    private BrokerRepository brokerRepository;
+    private ShareholderRepository shareholderRepository;
+    private Matcher matcher;
+
     public ApplicationServiceResponse deleteOrder(DeleteOrderRq req) {
+        validateDeleteOrderRq(req);
         return new ApplicationServiceResponse(null, null);
     }
 
     public ApplicationServiceResponse addLimitOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
         return new ApplicationServiceResponse(null, null);
     }
     
     public ApplicationServiceResponse updateLimitOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
+        validateUpdateOrderRq(req);
         return new ApplicationServiceResponse(null, null);
     }
     
     public ApplicationServiceResponse addIcebergOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
         return new ApplicationServiceResponse(null, null);
     }
     
     public ApplicationServiceResponse updateIcebergOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
+        validateUpdateOrderRq(req);
         return new ApplicationServiceResponse(null, null);
     }
 
     public ApplicationServiceResponse addStopLimitOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
         return new ApplicationServiceResponse(null, null);
     }
     
     public ApplicationServiceResponse updateStopLimitOrder(EnterOrderRq req) {
+        generalEnterOrderValidation(req);
+        validateUpdateOrderRq(req);
         return new ApplicationServiceResponse(null, null);
+    }
+
+    private void validateDeleteOrderRq(DeleteOrderRq deleteOrderRq) {
+        List<String> errors = deleteOrderRq.validateYourFields();
+        if (!securityRepository.isThereSecurityWithIsin(deleteOrderRq.getSecurityIsin()))
+            errors.add(Message.UNKNOWN_SECURITY_ISIN);
+        if (!errors.isEmpty())
+            throw new InvalidRequestException(errors);
+        Security security = securityRepository.findSecurityByIsin(deleteOrderRq.getSecurityIsin());
+        if (!security.isThereOrderWithId(deleteOrderRq.getSide(), deleteOrderRq.getOrderId()))
+            throw new InvalidRequestException(Message.ORDER_ID_NOT_FOUND);
+    }
+
+    private void generalEnterOrderValidation(EnterOrderRq enterOrderRq) {
+        List<String> errors = enterOrderRq.validateYourFields();
+        if (!securityRepository.isThereSecurityWithIsin(enterOrderRq.getSecurityIsin()))
+            errors.add(Message.UNKNOWN_SECURITY_ISIN);
+        else {
+            Security security = securityRepository.findSecurityByIsin(enterOrderRq.getSecurityIsin());
+            errors.addAll(security.checkLotAndTickSize(enterOrderRq));
+        }
+        if (!brokerRepository.isThereBrokerWithId(enterOrderRq.getBrokerId()))
+            errors.add(Message.UNKNOWN_BROKER_ID);
+        if (!shareholderRepository.isThereShareholderWithId(enterOrderRq.getShareholderId())) 
+            errors.add(Message.UNKNOWN_SHAREHOLDER_ID);
+        if (!errors.isEmpty())
+            throw new InvalidRequestException(errors);
+    }
+    
+    private void validateUpdateOrderRq (EnterOrderRq updateOrderRq) {
+        try {
+            Security security = securityRepository.findSecurityByIsin(updateOrderRq.getSecurityIsin());
+            Order order = security.findByOrderId(updateOrderRq.getSide(), updateOrderRq.getOrderId());
+            order.checkNewPeakSize(updateOrderRq.getPeakSize());
+            order.checkNewMinimumExecutionQuantity(updateOrderRq.getMinimumExecutionQuantity());
+            order.checkNewStopLimitPrice(updateOrderRq.getStopPrice());
+        }
+        catch (NotFoundException exp) {
+            throw new InvalidRequestException(Message.ORDER_ID_NOT_FOUND);
+        }
+        catch (InvalidIcebergPeakSizeException exp) {
+            throw new InvalidRequestException(Message.CANNOT_SPECIFY_0_PEAK_SIZE_FOR_A_ICEBERG_ORDER);
+        }
+        catch (InvalidPeakSizeException exp) {
+            throw new InvalidRequestException(Message.INVALID_PEAK_SIZE);
+        }
+        catch (UpdateMinimumExecutionQuantityException exp) {
+            throw new InvalidRequestException(Message.CANNOT_UPDATE_MINIMUM_EXECUTION_QUANTITY);
+        }
+        catch (InvalidStopLimitPriceException exp){
+            throw new InvalidRequestException(Message.INVALID_STOP_LIMIT_UPDATE_PRICE);
+        }
     }
 }
--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
@@ -1,13 +1,9 @@
 package ir.ramtung.tinyme.domain.service;
 
-import ir.ramtung.tinyme.domain.entity.*;
-import ir.ramtung.tinyme.domain.exception.InvalidIcebergPeakSizeException;
-import ir.ramtung.tinyme.domain.exception.InvalidPeakSizeException;
-import ir.ramtung.tinyme.domain.exception.NotFoundException;
-import ir.ramtung.tinyme.domain.exception.UpdateMinimumExecutionQuantityException;
-import ir.ramtung.tinyme.domain.exception.InvalidStopLimitPriceException;
 import ir.ramtung.tinyme.messaging.Message;
 import ir.ramtung.tinyme.messaging.exception.InvalidRequestException;
+import ir.ramtung.tinyme.domain.entity.MatchResult;
+import ir.ramtung.tinyme.domain.entity.MatchingOutcome;
 import ir.ramtung.tinyme.messaging.EventPublisher;
 import ir.ramtung.tinyme.messaging.TradeDTO;
 import ir.ramtung.tinyme.messaging.event.*;
@@ -15,9 +11,6 @@ import ir.ramtung.tinyme.messaging.request.BaseOrderRq;
 import ir.ramtung.tinyme.messaging.request.DeleteOrderRq;
 import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
 import ir.ramtung.tinyme.messaging.request.OrderEntryType;
-import ir.ramtung.tinyme.repository.BrokerRepository;
-import ir.ramtung.tinyme.repository.SecurityRepository;
-import ir.ramtung.tinyme.repository.ShareholderRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -26,19 +19,11 @@ import java.util.stream.Collectors;
 
 @Service
 public class OrderHandler {
-    SecurityRepository securityRepository;
-    BrokerRepository brokerRepository;
-    ShareholderRepository shareholderRepository;
     EventPublisher eventPublisher;
-    Matcher matcher;
     ApplicationServices services;
 
-    public OrderHandler(ApplicationServices services, SecurityRepository securityRepository, BrokerRepository brokerRepository, ShareholderRepository shareholderRepository, EventPublisher eventPublisher, Matcher matcher) {
-        this.securityRepository = securityRepository;
-        this.brokerRepository = brokerRepository;
-        this.shareholderRepository = shareholderRepository;
+    public OrderHandler(ApplicationServices services, EventPublisher eventPublisher) {
         this.eventPublisher = eventPublisher;
-        this.matcher = matcher;
         this.services = services;
     }
 
@@ -66,7 +51,7 @@ public class OrderHandler {
         if(req instanceof DeleteOrderRq deleteReq) {
             return callDeleteServices(deleteReq);
         }
-        
+
         if(req instanceof EnterOrderRq enterReq) {
             OrderEntryType type = enterReq.getRequestType();
             if (type == OrderEntryType.NEW_ORDER) {
@@ -105,41 +90,6 @@ public class OrderHandler {
         else {
             return services.updateLimitOrder(req);
         }
-    }
-
-    private List<MatchResult> runEnterOrderRq(EnterOrderRq enterOrderRq) {
-        Order tempOrder = createTempOrderByEnterOrderRq(enterOrderRq);
-        if (enterOrderRq.getRequestType() == OrderEntryType.NEW_ORDER)
-            return tempOrder.getSecurity().addNewOrder(tempOrder, matcher);
-        else if (enterOrderRq.getStopPrice() != 0)
-            return tempOrder.getSecurity().updateSloOrder((StopLimitOrder) tempOrder, matcher);
-        else
-            return tempOrder.getSecurity().updateOrder(tempOrder, matcher);
-    }
-
-    private Order createTempOrderByEnterOrderRq(EnterOrderRq enterOrderRq) {
-        Broker broker = brokerRepository.findBrokerById(enterOrderRq.getBrokerId());
-        Shareholder shareholder = shareholderRepository.findShareholderById(enterOrderRq.getShareholderId());
-        Security security = securityRepository.findSecurityByIsin(enterOrderRq.getSecurityIsin());
-        
-        if (enterOrderRq.getStopPrice() != 0) 
-            return new StopLimitOrder(
-                enterOrderRq.getOrderId(), security, enterOrderRq.getSide(), 
-                enterOrderRq.getQuantity(), enterOrderRq.getPrice(), broker, 
-                shareholder, enterOrderRq.getStopPrice()
-            );
-        else if (enterOrderRq.getPeakSize() != 0)
-            return new IcebergOrder(
-                enterOrderRq.getOrderId(), security, enterOrderRq.getSide(),
-                enterOrderRq.getQuantity(), enterOrderRq.getMinimumExecutionQuantity(), 
-                enterOrderRq.getPrice(), broker, shareholder, enterOrderRq.getEntryTime(), enterOrderRq.getPeakSize()
-            );
-        else
-            return new Order(
-                enterOrderRq.getOrderId(), security, enterOrderRq.getSide(),
-                enterOrderRq.getQuantity(), enterOrderRq.getMinimumExecutionQuantity(), 
-                enterOrderRq.getPrice(), broker, shareholder, enterOrderRq.getEntryTime()
-            );
     }
 
     private void publishApplicationServiceResponse(ApplicationServiceResponse response, BaseOrderRq enterOrderRq) {
@@ -183,62 +133,5 @@ public class OrderHandler {
         if (!matchResult.trades().isEmpty()) 
             events.add(new OrderExecutedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId(), matchResult.trades().stream().map(TradeDTO::new).collect(Collectors.toList())));
         return events;
-    }
-
-    private void validateEnterOrderRq(EnterOrderRq enterOrderRq) {
-        generalEnterOrderValidation(enterOrderRq);
-        if (enterOrderRq.getRequestType() == OrderEntryType.UPDATE_ORDER)
-            validateUpdateOrderRq(enterOrderRq, securityRepository.findSecurityByIsin(enterOrderRq.getSecurityIsin()));
-    }
-
-    private void generalEnterOrderValidation(EnterOrderRq enterOrderRq) {
-        List<String> errors = enterOrderRq.validateYourFields();
-        if (!securityRepository.isThereSecurityWithIsin(enterOrderRq.getSecurityIsin()))
-            errors.add(Message.UNKNOWN_SECURITY_ISIN);
-        else {
-            Security security = securityRepository.findSecurityByIsin(enterOrderRq.getSecurityIsin());
-            errors.addAll(security.checkLotAndTickSize(enterOrderRq));
-        }
-        if (!brokerRepository.isThereBrokerWithId(enterOrderRq.getBrokerId()))
-            errors.add(Message.UNKNOWN_BROKER_ID);
-        if (!shareholderRepository.isThereShareholderWithId(enterOrderRq.getShareholderId())) 
-            errors.add(Message.UNKNOWN_SHAREHOLDER_ID);
-        if (!errors.isEmpty())
-            throw new InvalidRequestException(errors);
-    }
-
-    private void validateUpdateOrderRq(EnterOrderRq updateOrderRq, Security security) {
-        try {
-            Order order = security.findByOrderId(updateOrderRq.getSide(), updateOrderRq.getOrderId());
-            order.checkNewPeakSize(updateOrderRq.getPeakSize());
-            order.checkNewMinimumExecutionQuantity(updateOrderRq.getMinimumExecutionQuantity());
-            order.checkNewStopLimitPrice(updateOrderRq.getStopPrice());
-        }
-        catch (NotFoundException exp) {
-            throw new InvalidRequestException(Message.ORDER_ID_NOT_FOUND);
-        }
-        catch (InvalidIcebergPeakSizeException exp) {
-            throw new InvalidRequestException(Message.CANNOT_SPECIFY_0_PEAK_SIZE_FOR_A_ICEBERG_ORDER);
-        }
-        catch (InvalidPeakSizeException exp) {
-            throw new InvalidRequestException(Message.INVALID_PEAK_SIZE);
-        }
-        catch (UpdateMinimumExecutionQuantityException exp) {
-            throw new InvalidRequestException(Message.CANNOT_UPDATE_MINIMUM_EXECUTION_QUANTITY);
-        }
-        catch (InvalidStopLimitPriceException exp){
-            throw new InvalidRequestException(Message.INVALID_STOP_LIMIT_UPDATE_PRICE);
-        }
-    }
-
-    private void validateDeleteOrderRq(DeleteOrderRq deleteOrderRq) {
-        List<String> errors = deleteOrderRq.validateYourFields();
-        if (!securityRepository.isThereSecurityWithIsin(deleteOrderRq.getSecurityIsin()))
-            errors.add(Message.UNKNOWN_SECURITY_ISIN);
-        if (!errors.isEmpty())
-            throw new InvalidRequestException(errors);
-        Security security = securityRepository.findSecurityByIsin(deleteOrderRq.getSecurityIsin());
-        if (!security.isThereOrderWithId(deleteOrderRq.getSide(), deleteOrderRq.getOrderId()))
-            throw new InvalidRequestException(Message.ORDER_ID_NOT_FOUND);
     }
 }
