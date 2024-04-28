819b89a3aa33600045ce95e3ba9c2c51881cff17
Shahriar-0 <attarshahriar@gmail.com>
2024-04-28 08:48:22 +0330

    clean matcher and orderHandler

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
@@ -4,66 +4,64 @@ import ir.ramtung.tinyme.domain.entity.*;
 import ir.ramtung.tinyme.domain.exception.NotEnoughCreditException;
 import ir.ramtung.tinyme.domain.exception.NotEnoughExecutionException;
 import ir.ramtung.tinyme.domain.exception.NotFoundException;
-
-import org.springframework.stereotype.Service;
-
 import java.util.LinkedList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class Matcher {
-    public List<Trade> match(Order newOrder) {
-        OrderBook orderBook = newOrder.getSecurity().getOrderBook();
-        LinkedList<Trade> trades = new LinkedList<>();
-        try {
-            while (hasOrderToMatch(newOrder, orderBook)) {
-                Order matchingOrder = orderBook.findOrderToMatchWith(newOrder);    
-                Trade trade = new Trade(newOrder, matchingOrder);
-                trade.confirm();
-                trades.add(trade);
-            }
-            return trades;
-        }
-        catch (NotFoundException exp) {
-            return trades;
-        } 
-        catch (NotEnoughCreditException exp) {
-            rollbackTrades(trades);
-            throw exp;
-        }
+
+	public List<Trade> match(Order newOrder) {
+		OrderBook orderBook = newOrder.getSecurity().getOrderBook();
+		LinkedList<Trade> trades = new LinkedList<>();
+		try {
+			while (hasOrderToMatch(newOrder, orderBook)) {
+				tryToTrade(newOrder, orderBook, trades);
+			}
+			return trades;
+		} catch (NotFoundException exp) {
+			return trades;
+		} catch (NotEnoughCreditException exp) {
+			rollbackTrades(trades);
+			throw exp;
+		}
+	}
+
+    private void tryToTrade(Order newOrder, OrderBook orderBook, LinkedList<Trade> trades) {
+        Order matchingOrder = orderBook.findOrderToMatchWith(newOrder);
+        Trade trade = new Trade(newOrder, matchingOrder);
+        trade.confirm();
+        trades.add(trade);
     }
 
-    private boolean hasOrderToMatch(Order newOrder, OrderBook orderBook) {
-        return (orderBook.hasOrderOfType(newOrder.getSide().opposite())) && (newOrder.getQuantity() > 0);
-    }
+	private boolean hasOrderToMatch(Order newOrder, OrderBook orderBook) {
+		return (orderBook.hasOrderOfType(newOrder.getSide().opposite())) && (newOrder.getQuantity() > 0);
+	}
 
-    private void rollbackTrades(List<Trade> trades) {
-        for (int i = trades.size() - 1; i >= 0; i--)
-            trades.get(i).rollback();
-    }
+	private void rollbackTrades(List<Trade> trades) {
+		trades.reversed().forEach(Trade::rollback);
+	}
 
-    public MatchResult execute(Order order) {
-        List<Trade> trades = new LinkedList<>();
-        try {
-            trades = match(order);
-            order.checkExecutionQuantity(sumOfExecutionQuantity(trades));
-            order.addYourselfToQueue();
-            return MatchResult.executed(order, trades);
-        }
-        catch (NotEnoughCreditException exp) {
-            rollbackTrades(trades);
-            return MatchResult.notEnoughCredit();
-        }
-        catch (NotEnoughExecutionException exp) {
-            rollbackTrades(trades);
-            return MatchResult.notEnoughExecution();
-        }
-    }
+	public MatchResult execute(Order order) {
+		List<Trade> trades = new LinkedList<>();
+		
+		try {
+			trades = match(order);
+			order.checkExecutionQuantity(sumOfExecutionQuantity(trades));
+			order.addYourselfToQueue();
+			return MatchResult.executed(order, trades);
+		} catch (NotEnoughCreditException exp) {
+			rollbackTrades(trades);
+			return MatchResult.notEnoughCredit();
+		} catch (NotEnoughExecutionException exp) {
+			rollbackTrades(trades);
+			return MatchResult.notEnoughExecution();
+		}
+	}
 
-    private int sumOfExecutionQuantity(List<Trade> trades) {
-        int quantitySum = 0;
-        for (Trade trade: trades) 
-            quantitySum += trade.getQuantity();
-        return quantitySum;
-    }
+	private int sumOfExecutionQuantity(List<Trade> trades) {
+		int quantitySum = 0;
+		for (Trade trade : trades) quantitySum += trade.getQuantity();
+		return quantitySum;
+	}
 }
--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/OrderHandler.java
@@ -1,163 +1,184 @@
 package ir.ramtung.tinyme.domain.service;
 
-import ir.ramtung.tinyme.messaging.Message;
-import ir.ramtung.tinyme.messaging.exception.InvalidRequestException;
 import ir.ramtung.tinyme.domain.entity.MatchingOutcome;
 import ir.ramtung.tinyme.domain.entity.Trade;
 import ir.ramtung.tinyme.messaging.EventPublisher;
+import ir.ramtung.tinyme.messaging.Message;
 import ir.ramtung.tinyme.messaging.TradeDTO;
 import ir.ramtung.tinyme.messaging.event.*;
+import ir.ramtung.tinyme.messaging.exception.InvalidRequestException;
 import ir.ramtung.tinyme.messaging.request.BaseOrderRq;
 import ir.ramtung.tinyme.messaging.request.DeleteOrderRq;
 import ir.ramtung.tinyme.messaging.request.EnterOrderRq;
 import ir.ramtung.tinyme.messaging.request.OrderEntryType;
-import org.springframework.stereotype.Service;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
 @Service
 public class OrderHandler {
-    EventPublisher eventPublisher;
-    ApplicationServices services;
 
-    public OrderHandler(ApplicationServices services, EventPublisher eventPublisher) {
-        this.eventPublisher = eventPublisher;
-        this.services = services;
-    }
+	EventPublisher eventPublisher;
+	ApplicationServices services;
 
-    public void handleEnterOrder(EnterOrderRq enterOrderRq) {
-        try {
-            ApplicationServiceResponse response = callService(enterOrderRq);
-            publishApplicationServiceResponse(response);
-        } 
-        catch (InvalidRequestException ex) {
-            eventPublisher.publish(new OrderRejectedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId(), ex.getReasons()));
-        }
-    }
+	public OrderHandler(ApplicationServices services, EventPublisher eventPublisher) {
+		this.eventPublisher = eventPublisher;
+		this.services = services;
+	}
 
-    public void handleDeleteOrder(DeleteOrderRq deleteOrderRq) {
-        try {
-            ApplicationServiceResponse response = callService(deleteOrderRq);
-            publishApplicationServiceResponse(response);
-        } 
-        catch (InvalidRequestException ex) {
-            eventPublisher.publish(new OrderRejectedEvent(deleteOrderRq.getRequestId(), deleteOrderRq.getOrderId(), ex.getReasons()));
-        }
-    }
+	public void handleEnterOrder(EnterOrderRq enterOrderRq) {
+		try {
+			ApplicationServiceResponse response = callService(enterOrderRq);
+			publishApplicationServiceResponse(response);
+		} catch (InvalidRequestException ex) {
+			eventPublisher.publish(
+				new OrderRejectedEvent(enterOrderRq.getRequestId(), enterOrderRq.getOrderId(), ex.getReasons())
+			);
+		}
+	}
 
-    private ApplicationServiceResponse callService(BaseOrderRq req) {
-        if(req instanceof DeleteOrderRq deleteReq) {
-            return callDeleteServices(deleteReq);
-        }
+	public void handleDeleteOrder(DeleteOrderRq deleteOrderRq) {
+		try {
+			ApplicationServiceResponse response = callService(deleteOrderRq);
+			publishApplicationServiceResponse(response);
+		} catch (InvalidRequestException ex) {
+			eventPublisher.publish(
+				new OrderRejectedEvent(deleteOrderRq.getRequestId(), deleteOrderRq.getOrderId(), ex.getReasons())
+			);
+		}
+	}
 
-        if(req instanceof EnterOrderRq enterReq) {
-            OrderEntryType type = enterReq.getRequestType();
-            if (type == OrderEntryType.NEW_ORDER) {
-                return callAddServices(enterReq);
-            }
-            else if (type == OrderEntryType.UPDATE_ORDER) {
-                return callUpdateServices(enterReq);
-            }
-        }
-        throw new InvalidRequestException(Message.UNKNOWN_REQUEST_TYPE);
-    }
+	private ApplicationServiceResponse callService(BaseOrderRq req) {
+		if (req instanceof DeleteOrderRq deleteReq) {
+			return callDeleteServices(deleteReq);
+		}
 
-    private ApplicationServiceResponse callDeleteServices(DeleteOrderRq req) {
-        return services.deleteOrder(req);
-    }
+		if (req instanceof EnterOrderRq enterReq) {
+			OrderEntryType type = enterReq.getRequestType();
+			if (type == OrderEntryType.NEW_ORDER) {
+				return callAddServices(enterReq);
+			} else if (type == OrderEntryType.UPDATE_ORDER) {
+				return callUpdateServices(enterReq);
+			}
+		}
+		throw new InvalidRequestException(Message.UNKNOWN_REQUEST_TYPE);
+	}
 
-    private ApplicationServiceResponse callAddServices(EnterOrderRq req) {
-        if (req.getStopPrice() != 0) {
-            return services.addStopLimitOrder(req);
-        }
-        else if (req.getPeakSize() != 0) {
-            return services.addIcebergOrder(req);
-        }
-        else {
-            return services.addLimitOrder(req);
-        }
-    }
+	private ApplicationServiceResponse callDeleteServices(DeleteOrderRq req) {
+		return services.deleteOrder(req);
+	}
 
-    private ApplicationServiceResponse callUpdateServices(EnterOrderRq req) {
-        if (req.getStopPrice() != 0) {
-            return services.updateStopLimitOrder(req);
-        }
-        else if (req.getPeakSize() != 0) {
-            return services.updateIcebergOrder(req);
-        }
-        else {
-            return services.updateLimitOrder(req);
-        }
-    }
+	private ApplicationServiceResponse callAddServices(EnterOrderRq req) {
+		if (req.getStopPrice() != 0) {
+			return services.addStopLimitOrder(req);
+		} else if (req.getPeakSize() != 0) {
+			return services.addIcebergOrder(req);
+		} else {
+			return services.addLimitOrder(req);
+		}
+	}
 
-    private void publishApplicationServiceResponse(ApplicationServiceResponse response) {
-        List<Event> events = createEvents(response);
-        events.forEach(event -> eventPublisher.publish(event));
-    }
+	private ApplicationServiceResponse callUpdateServices(EnterOrderRq req) {
+		if (req.getStopPrice() != 0) {
+			return services.updateStopLimitOrder(req);
+		} else if (req.getPeakSize() != 0) {
+			return services.updateIcebergOrder(req);
+		} else {
+			return services.updateLimitOrder(req);
+		}
+	}
 
-    private List<Event> createEvents(ApplicationServiceResponse response) {
-        if (response.isTypeDelete()) {
-            return List.of(new OrderDeletedEvent(response.getRequestId(), response.getOrderId()));
-        }
-        List<Event> events = createFirstMatchResultEvents(response);
-        events.addAll(createActivatedEvents(response));
-        return events;
-    }
+	private void publishApplicationServiceResponse(ApplicationServiceResponse response) {
+		List<Event> events = createEvents(response);
+		events.forEach(event -> eventPublisher.publish(event));
+	}
 
-    private List<Event> createFirstMatchResultEvents(ApplicationServiceResponse response) {
-        if (response.isSuccessful(0)) {
-            return createSuccessEvents(response);
-        }
-        else {
-            return createRejectedEvents(response);
-        }
-    }
+	private List<Event> createEvents(ApplicationServiceResponse response) {
+		if (response.isTypeDelete()) {
+			return List.of(new OrderDeletedEvent(response.getRequestId(), response.getOrderId()));
+		}
 
-    private List<Event> createActivatedEvents(ApplicationServiceResponse response) {
-        List<Event> events = new LinkedList<>();
-        int numOfMatchResults = response.getMatchResults().size();
-        for(int i = 1; i < numOfMatchResults; i++) {
-            events.add(new OrderActivatedEvent(response.getOrderId(i)));
-            if((response.hasTrades(i))) {
-                events.add(createExecutedEvent(response.getOrderId(i), response.getTrades(i)));
-            }
-        }
-        return events;
-    }
+		List<Event> events = createFirstMatchResultEvents(response);
+		events.addAll(createActivatedEvents(response));
+		return events;
+	}
 
-    private List<Event> createSuccessEvents(ApplicationServiceResponse response) {
-        List<Event> events = new LinkedList<>();
-        if (response.isTypeAdd()) {
-            events.add(new OrderAcceptedEvent(response.getRequestId(), response.getOrderId()));
-        }
-        else {
-            events.add(new OrderUpdatedEvent(response.getRequestId(), response.getOrderId()));
-        }
-        if (response.hasTrades(0)) {
-            events.add(createExecutedEvent(response.getRequestId(), response.getOrderId(), response.getTrades(0)));
-        }
-        return events;
-    }
+	private List<Event> createFirstMatchResultEvents(ApplicationServiceResponse response) {
+		if (response.isSuccessful(0)) {
+			return createSuccessEvents(response);
+		} else {
+			return createRejectedEvents(response);
+		}
+	}
 
-    private List<Event> createRejectedEvents(ApplicationServiceResponse response) {
-        List<Event> events = new LinkedList<>();
-        MatchingOutcome outcome = response.getOutcome(0);
-        if (outcome == MatchingOutcome.NOT_ENOUGH_CREDIT) 
-            events.add(new OrderRejectedEvent(response.getRequestId(), response.getOrderId(), List.of(Message.BUYER_HAS_NOT_ENOUGH_CREDIT)));
-        else if (outcome == MatchingOutcome.NOT_ENOUGH_POSITIONS) 
-            events.add(new OrderRejectedEvent(response.getRequestId(), response.getOrderId(), List.of(Message.SELLER_HAS_NOT_ENOUGH_POSITIONS)));
-        else if (outcome == MatchingOutcome.NOT_ENOUGH_EXECUTION)
-            events.add(new OrderRejectedEvent(response.getRequestId(), response.getOrderId(), List.of(Message.MINIMUM_EXECUTION_QUANTITY_NOT_MET)));
-        return events;
-    }
+	private List<Event> createActivatedEvents(ApplicationServiceResponse response) {
+		List<Event> events = new LinkedList<>();
+		int numOfMatchResults = response.getMatchResults().size();
 
-    private Event createExecutedEvent(long reqId, long orderId, List<Trade> trades) {
-        return new OrderExecutedEvent(reqId, orderId, trades.stream().map(TradeDTO::new).collect(Collectors.toList()));
-    }
+		for (int i = 1; i < numOfMatchResults; i++) {
+			events.add(new OrderActivatedEvent(response.getOrderId(i)));
+			if ((response.hasTrades(i))) {
+				events.add(createExecutedEvent(response.getOrderId(i), response.getTrades(i)));
+			}
+		}
 
-    private Event createExecutedEvent(long orderId, List<Trade> trades) {
-        return new OrderExecutedEvent(orderId, trades.stream().map(TradeDTO::new).collect(Collectors.toList()));
-    }
+		return events;
+	}
+
+	private List<Event> createSuccessEvents(ApplicationServiceResponse response) {
+		List<Event> events = new LinkedList<>();
+
+		if (response.isTypeAdd()) {
+			events.add(new OrderAcceptedEvent(response.getRequestId(), response.getOrderId()));
+		} else {
+			events.add(new OrderUpdatedEvent(response.getRequestId(), response.getOrderId()));
+		}
+
+		if (response.hasTrades(0)) {
+			events.add(createExecutedEvent(response.getRequestId(), response.getOrderId(), response.getTrades(0)));
+		}
+
+		return events;
+	}
+
+	private List<Event> createRejectedEvents(ApplicationServiceResponse response) {
+		List<Event> events = new LinkedList<>();
+		MatchingOutcome outcome = response.getOutcome(0);
+
+		if (outcome == MatchingOutcome.NOT_ENOUGH_CREDIT) {
+			events.add(
+				new OrderRejectedEvent(
+					response.getRequestId(),
+					response.getOrderId(),
+					List.of(Message.BUYER_HAS_NOT_ENOUGH_CREDIT)
+				)
+			);
+		} else if (outcome == MatchingOutcome.NOT_ENOUGH_POSITIONS) {
+			events.add(
+				new OrderRejectedEvent(
+					response.getRequestId(),
+					response.getOrderId(),
+					List.of(Message.SELLER_HAS_NOT_ENOUGH_POSITIONS)
+				)
+			);
+		} else if (outcome == MatchingOutcome.NOT_ENOUGH_EXECUTION) {
+			events.add(
+				new OrderRejectedEvent(
+					response.getRequestId(),
+					response.getOrderId(),
+					List.of(Message.MINIMUM_EXECUTION_QUANTITY_NOT_MET)
+				)
+			);
+		}
+		return events;
+	}
+
+	private Event createExecutedEvent(long reqId, long orderId, List<Trade> trades) {
+		return new OrderExecutedEvent(reqId, orderId, trades.stream().map(TradeDTO::new).collect(Collectors.toList()));
+	}
+
+	private Event createExecutedEvent(long orderId, List<Trade> trades) {
+		return new OrderExecutedEvent(orderId, trades.stream().map(TradeDTO::new).collect(Collectors.toList()));
+	}
 }
