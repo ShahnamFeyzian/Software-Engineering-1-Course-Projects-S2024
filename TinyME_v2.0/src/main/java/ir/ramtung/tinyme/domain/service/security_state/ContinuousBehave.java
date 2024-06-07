a8563b6bf0873f9392e39ab30de28f7b1a48db10
Shahriar-0 <attarshahriar@gmail.com>
2024-06-07 19:19:43 +0330

    clean continuous behave a bit

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/security_state/ContinuousBehave.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/security_state/ContinuousBehave.java
@@ -1,11 +1,5 @@
 package ir.ramtung.tinyme.domain.service.security_state;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import ir.ramtung.tinyme.domain.entity.MatchResult;
 import ir.ramtung.tinyme.domain.entity.Order;
 import ir.ramtung.tinyme.domain.entity.OrderBook;
@@ -19,35 +13,42 @@ import ir.ramtung.tinyme.domain.service.Matcher;
 import ir.ramtung.tinyme.domain.service.controls.ControlResult;
 import ir.ramtung.tinyme.domain.service.controls.CreditControl;
 import ir.ramtung.tinyme.domain.service.controls.PositionControl;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ContinuousBehave implements SecurityBehave {
+
 	private PositionControl positionControl;
 	private CreditControl creditControl;
 	private Matcher matcher;
 
-    public ContinuousBehave(PositionControl positionControl, CreditControl creditControl, Matcher matcher) {
-        this.positionControl = positionControl;
-        this.creditControl = creditControl;
-        this.matcher = matcher;
-    }
+	public ContinuousBehave(PositionControl positionControl, CreditControl creditControl, Matcher matcher) {
+		this.positionControl = positionControl;
+		this.creditControl = creditControl;
+		this.matcher = matcher;
+	}
 
-    @Override
-    public List<SecurityStats> addNewOrder(Order newOrder, OrderBook orderBook, int lastTradePrice) {
-        if (positionControl.checkPositionForOrder(newOrder, orderBook) != ControlResult.OK) {
-			return new ArrayList<SecurityStats>(List.of(SituationalStats.createNotEnoughPositionsStats(newOrder.getOrderId())));
+	@Override
+	public List<SecurityStats> addNewOrder(Order newOrder, OrderBook orderBook, int lastTradePrice) {
+		if (positionControl.checkPositionForOrder(newOrder, orderBook) != ControlResult.OK) {
+			return new ArrayList<SecurityStats>(
+				List.of(SituationalStats.createNotEnoughPositionsStats(newOrder.getOrderId()))
+			);
 		}
 
-        if (newOrder instanceof StopLimitOrder newStopLimitOrder) {
+		if (newOrder instanceof StopLimitOrder newStopLimitOrder) {
 			return addNewStopLimitOrder(newStopLimitOrder, orderBook);
 		} else {
 			return addNewLimitOrder(newOrder, orderBook);
 		}
-    }
+	}
 
-    @Override
-    public List<SecurityStats> updateOrder(Order tempOrder, Order mainOrder, OrderBook orderBook, int lastTradePrice) {
-        boolean losesPriority = mainOrder.willPriorityLostInUpdate(tempOrder);
+	@Override
+	public List<SecurityStats> updateOrder(Order tempOrder, Order mainOrder, OrderBook orderBook, int lastTradePrice) {
+		boolean losesPriority = mainOrder.willPriorityLostInUpdate(tempOrder);
 		if (losesPriority) {
 			Order originalOrder = mainOrder.snapshot();
 			creditControl.updateCreditAtDelete(mainOrder);
@@ -56,45 +57,58 @@ public class ContinuousBehave implements SecurityBehave {
 			return reAddUpdatedOrder(mainOrder, originalOrder, orderBook);
 		} else {
 			mainOrder.updateFromTempOrder(tempOrder);
-		    return new ArrayList<SecurityStats>(List.of(SituationalStats.createUpdateOrderStats(mainOrder.getOrderId())));
+			return new ArrayList<SecurityStats>(
+				List.of(SituationalStats.createUpdateOrderStats(mainOrder.getOrderId()))
+			);
 		}
-    }
+	}
 
-    @Override
-    public List<SecurityStats> deleteOrder(Order targetOrder, OrderBook orderBook, int lastTradePrice) {
-        creditControl.updateCreditAtDelete(targetOrder);
+	@Override
+	public List<SecurityStats> deleteOrder(Order targetOrder, OrderBook orderBook, int lastTradePrice) {
+		creditControl.updateCreditAtDelete(targetOrder);
 		orderBook.removeOrder(targetOrder);
-        return new ArrayList<SecurityStats>(List.of(SituationalStats.createDeleteOrderStats(targetOrder.getOrderId())));
-    }
+		return new ArrayList<SecurityStats>(List.of(SituationalStats.createDeleteOrderStats(targetOrder.getOrderId())));
+	}
 
-    @Override
-    public List<SecurityStats> activateStopLimitOrders(OrderBook orderBook, int lastTradePrice) {
-        List<SecurityStats> stats = new LinkedList<>();
+	@Override
+	public List<SecurityStats> activateStopLimitOrders(OrderBook orderBook, int lastTradePrice) {
+		List<SecurityStats> stats = new LinkedList<>();
 		StopLimitOrder slo;
-        int currentLastTradePrice = lastTradePrice;
+		int currentLastTradePrice = lastTradePrice;
 
-		while ((slo = orderBook.getStopLimitOrder(currentLastTradePrice)) != null) {
+		slo = orderBook.getStopLimitOrder(currentLastTradePrice);
+
+		while (slo != null) {
 			creditControl.updateCreditAtDelete(slo);
 			stats.add(SituationalStats.createOrderActivatedStats(slo.getOrderId(), slo.getRequestId()));
 			Order activatedOrder = new Order(slo);
-            MatchResult result = matcher.continuousExecuting(activatedOrder, orderBook);
-            if (!result.trades().isEmpty()) {
-                stats.add(ExecuteStats.createContinuousExecuteStatsForActivatedOrder(result.trades(), activatedOrder.getOrderId(), slo.getRequestId()));
-                currentLastTradePrice = result.trades().getLast().getPrice();
-            }
+			MatchResult result = matcher.continuousExecuting(activatedOrder, orderBook);
+			if (!result.trades().isEmpty()) {
+				stats.add(
+					ExecuteStats.createContinuousExecuteStatsForActivatedOrder(
+						result.trades(),
+						activatedOrder.getOrderId(),
+						slo.getRequestId()
+					)
+				);
+				currentLastTradePrice = result.trades().getLast().getPrice();
+			}
+			slo = orderBook.getStopLimitOrder(currentLastTradePrice);
 		}
 
 		return stats;
-    }
+	}
 
-    @Override
-    public List<SecurityStats> changeMatchingState(OrderBook orderBook, int lastTradePrice, SecurityState newState) {
-        return new ArrayList<SecurityStats>(List.of(StateStats.createStateStats(SecurityState.CONTINUOUS, newState)));
-    }
-    
-    private List<SecurityStats> addNewStopLimitOrder(StopLimitOrder newOrder, OrderBook orderBook) {
+	@Override
+	public List<SecurityStats> changeMatchingState(OrderBook orderBook, int lastTradePrice, SecurityState newState) {
+		return new ArrayList<SecurityStats>(List.of(StateStats.createStateStats(SecurityState.CONTINUOUS, newState)));
+	}
+
+	private List<SecurityStats> addNewStopLimitOrder(StopLimitOrder newOrder, OrderBook orderBook) {
 		if (creditControl.checkCreditForBeingQueued(newOrder) != ControlResult.OK) {
-			return new ArrayList<SecurityStats>(List.of(SituationalStats.createNotEnoughCreditStats(newOrder.getOrderId())));
+			return new ArrayList<SecurityStats>(
+				List.of(SituationalStats.createNotEnoughCreditStats(newOrder.getOrderId()))
+			);
 		}
 
 		creditControl.updateCreditForBeingQueued(newOrder);
@@ -102,27 +116,35 @@ public class ContinuousBehave implements SecurityBehave {
 		return new ArrayList<SecurityStats>(List.of(SituationalStats.createAddOrderStats(newOrder.getOrderId())));
 	}
 
-    private List<SecurityStats> addNewLimitOrder(Order newOrder, OrderBook orderBook) {
+	private List<SecurityStats> addNewLimitOrder(Order newOrder, OrderBook orderBook) {
 		List<SecurityStats> stats = new ArrayList<>();
 		stats.add(SituationalStats.createAddOrderStats(newOrder.getOrderId()));
 
 		MatchResult newOrderMatchResult = matcher.continuousExecuting(newOrder, orderBook);
 		if (!newOrderMatchResult.isSuccessful()) {
-			stats.set(0, SituationalStats.createExecutionStatsFromUnsuccessfulMatchResult(newOrderMatchResult, newOrder.getOrderId()));
+			stats.set(
+				0,
+				SituationalStats.createExecutionStatsFromUnsuccessfulMatchResult(
+					newOrderMatchResult,
+					newOrder.getOrderId()
+				)
+			);
 		}
-		if(!newOrderMatchResult.trades().isEmpty()) {
+		if (!newOrderMatchResult.trades().isEmpty()) {
 			stats.add(ExecuteStats.createContinuousExecuteStats(newOrderMatchResult.trades(), newOrder.getOrderId()));
 		}
 		return stats;
 	}
 
-    private List<SecurityStats> reAddUpdatedOrder(Order updatedOrder, Order originalOrder, OrderBook orderBook) {
+	private List<SecurityStats> reAddUpdatedOrder(Order updatedOrder, Order originalOrder, OrderBook orderBook) {
 		if (positionControl.checkPositionForOrder(updatedOrder, orderBook) != ControlResult.OK) {
 			creditControl.updateCreditForBeingQueued(originalOrder);
 			orderBook.enqueue(originalOrder);
-			return new ArrayList<SecurityStats>(List.of(SituationalStats.createNotEnoughPositionsStats(originalOrder.getOrderId())));
+			return new ArrayList<SecurityStats>(
+				List.of(SituationalStats.createNotEnoughPositionsStats(originalOrder.getOrderId()))
+			);
 		}
-		
+
 		if (updatedOrder instanceof StopLimitOrder updatedSlo) {
 			StopLimitOrder originalSlo = (StopLimitOrder) originalOrder;
 			return reAddUpdatedSlo(updatedSlo, originalSlo, orderBook);
@@ -131,34 +153,48 @@ public class ContinuousBehave implements SecurityBehave {
 		}
 	}
 
-    private List<SecurityStats> reAddUpdatedSlo(StopLimitOrder updatedOrder,StopLimitOrder originalOrder, OrderBook orderBook) {
-			if (creditControl.checkCreditForBeingQueued(updatedOrder) != ControlResult.OK) {
-				creditControl.updateCreditForBeingQueued(originalOrder);
-				orderBook.enqueue(originalOrder);
-				return new ArrayList<SecurityStats>(List.of(SituationalStats.createNotEnoughCreditStats(originalOrder.getOrderId())));
-			}
+	private List<SecurityStats> reAddUpdatedSlo(
+		StopLimitOrder updatedOrder,
+		StopLimitOrder originalOrder,
+		OrderBook orderBook
+	) {
+		if (creditControl.checkCreditForBeingQueued(updatedOrder) != ControlResult.OK) {
+			creditControl.updateCreditForBeingQueued(originalOrder);
+			orderBook.enqueue(originalOrder);
+			return new ArrayList<SecurityStats>(
+				List.of(SituationalStats.createNotEnoughCreditStats(originalOrder.getOrderId()))
+			);
+		}
 
-			creditControl.updateCreditForBeingQueued(updatedOrder);
-			orderBook.enqueue(updatedOrder);
+		creditControl.updateCreditForBeingQueued(updatedOrder);
+		orderBook.enqueue(updatedOrder);
 
-			List<SecurityStats> stats = new LinkedList<>();
-			stats.add(SituationalStats.createUpdateOrderStats(originalOrder.getOrderId()));
-			return stats;
-	}
-
-    private List<SecurityStats> reAddActiveOrder(Order updatedOrder, Order originalOrder, OrderBook orderBook) {
 		List<SecurityStats> stats = new LinkedList<>();
 		stats.add(SituationalStats.createUpdateOrderStats(originalOrder.getOrderId()));
-		
+		return stats;
+	}
+
+	private List<SecurityStats> reAddActiveOrder(Order updatedOrder, Order originalOrder, OrderBook orderBook) {
+		List<SecurityStats> stats = new LinkedList<>();
+		stats.add(SituationalStats.createUpdateOrderStats(originalOrder.getOrderId()));
+
 		MatchResult updatedOrderResult = matcher.continuousExecuting(updatedOrder, orderBook);
 
 		if (!updatedOrderResult.isSuccessful()) {
 			creditControl.updateCreditForBeingQueued(originalOrder);
 			orderBook.enqueue(originalOrder);
-			stats.set(0, SituationalStats.createExecutionStatsFromUnsuccessfulMatchResult(updatedOrderResult, originalOrder.getOrderId()));
-		} 
+			stats.set(
+				0,
+				SituationalStats.createExecutionStatsFromUnsuccessfulMatchResult(
+					updatedOrderResult,
+					originalOrder.getOrderId()
+				)
+			);
+		}
 		if (!updatedOrderResult.trades().isEmpty()) {
-			stats.add(ExecuteStats.createContinuousExecuteStats(updatedOrderResult.trades(), originalOrder.getOrderId()));
+			stats.add(
+				ExecuteStats.createContinuousExecuteStats(updatedOrderResult.trades(), originalOrder.getOrderId())
+			);
 		}
 
 		return stats;
