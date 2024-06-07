4ebb2160d2d8140598d275e164bda662a83ad376
Shahriar-0 <attarshahriar@gmail.com>
2024-06-07 19:37:22 +0330

    chore: Refactor Matcher class for improved readability

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
@@ -5,7 +5,6 @@ import ir.ramtung.tinyme.domain.service.controls.AuctionMatchingControl;
 import ir.ramtung.tinyme.domain.service.controls.ContinuousMatchingControl;
 import ir.ramtung.tinyme.domain.service.controls.ControlResult;
 import ir.ramtung.tinyme.domain.service.controls.MatchingControl;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Matcher {
+
 	private MatchingControl continuousMatchingControl;
 	private MatchingControl auctionMatchingControl;
 
@@ -26,43 +26,74 @@ public class Matcher {
 	}
 
 	public int calcOpeningAuctionPrice(OrderBook orderBook, int lastTradePrice) {
-		if(!hasOrderForAuction(orderBook)) {
+		if (!hasOrderForAuction(orderBook)) {
 			return lastTradePrice;
 		}
 
-		int maxTradableQuantity = 0;
-		int openingPrice = lastTradePrice; 
 		int minPrice = orderBook.getLowestPriorityActiveOrder(Side.BUY).getPrice();
 		int maxPrice = orderBook.getLowestPriorityActiveOrder(Side.SELL).getPrice();
-		
+
+		int maxTradableQuantity = 0;
+		int openingPrice = lastTradePrice;
+
 		for (int price = minPrice; price <= maxPrice; price++) {
 			int currentTradableQuantity = calcTradableQuantity(orderBook, price);
-			if (shouldUpdateOpeningAuctionPrice(openingPrice, price, lastTradePrice, maxTradableQuantity, currentTradableQuantity)) {
+			if (
+				shouldUpdateOpeningAuctionPrice(
+					openingPrice,
+					price,
+					lastTradePrice,
+					maxTradableQuantity,
+					currentTradableQuantity
+				)
+			) {
 				openingPrice = price;
 				maxTradableQuantity = currentTradableQuantity;
 			}
 		}
-		
+
 		return openingPrice;
 	}
 
-	private boolean shouldUpdateOpeningAuctionPrice(int openingPrice, int newOpeningPrice, int lastTradePrice, int openingTradableQuantity, int newOpeningTradableQuantity) {
-		return newOpeningTradableQuantity > openingTradableQuantity || 
-				(newOpeningTradableQuantity == openingTradableQuantity && 
-				Math.abs(newOpeningPrice - lastTradePrice) < Math.abs(openingPrice - lastTradePrice));
+	private boolean shouldUpdateOpeningAuctionPrice(
+		int openingPrice,
+		int newOpeningPrice,
+		int lastTradePrice,
+		int openingTradableQuantity,
+		int newOpeningTradableQuantity
+	) {
+		return (
+			newOpeningTradableQuantity > openingTradableQuantity ||
+			(
+				newOpeningTradableQuantity == openingTradableQuantity &&
+				Math.abs(newOpeningPrice - lastTradePrice) < Math.abs(openingPrice - lastTradePrice)
+			)
+		);
 	}
 
 	public int calcTradableQuantity(OrderBook orderBook, int openingPrice) {
 		int buysQuantity = calcTradableQuantityInQueue(orderBook.getBuyQueue(), openingPrice);
-		int sellsQuantity = calcTradableQuantityInQueue(orderBook.getSellQueue(), openingPrice);;
+		int sellsQuantity = calcTradableQuantityInQueue(orderBook.getSellQueue(), openingPrice);
 		return Math.min(buysQuantity, sellsQuantity);
 	}
 
 	private int calcTradableQuantityInQueue(List<Order> queue, int price) {
-		return queue.stream()
-			 		.filter(order -> order.canTradeWithPrice(price))
-			 		.mapToInt(Order::getTotalQuantity)
-			 		.sum();
+		return queue.stream().filter(order -> order.canTradeWithPrice(price)).mapToInt(Order::getTotalQuantity).sum();
+	}
+
+	private void controllerMatch(Trade trade, OrderBook orderBook, MatchingControl control, List<Trade> trades) {
+		control.actionAtMatch(trade, orderBook);
+		trades.add(trade);
+	}
+
+	private MatchResult controllerFailedBeforeMatch(
+		MatchingControl control,
+		List<Trade> trades,
+		OrderBook orderBook,
+		ControlResult controlResult
+	) {
+		control.actionAtFailedBeforeMatch(trades, orderBook);
+		return MatchResult.createFromControlResult(controlResult);
 	}
 
 	public MatchResult continuousMatch(Order order, OrderBook orderBook) {
@@ -73,12 +104,11 @@ public class Matcher {
 		while ((matchingOrder = getMatchingOrder(order, orderBook)) != null) {
 			Trade trade = createTradeForContinuousMatching(order, matchingOrder);
 			controlResult = continuousMatchingControl.checkBeforeMatch(trade);
+			
 			if (controlResult == ControlResult.OK) {
-				continuousMatchingControl.actionAtMatch(trade, orderBook);
-				trades.add(trade);
+				controllerMatch(trade, orderBook, continuousMatchingControl, trades);
 			} else {
-				continuousMatchingControl.actionAtFailedBeforeMatch(trades, orderBook);
-				return MatchResult.createFromControlResult(controlResult);
+				return controllerFailedBeforeMatch(continuousMatchingControl, trades, orderBook, controlResult);
 			}
 		}
 
@@ -95,17 +125,15 @@ public class Matcher {
 		List<Trade> trades = new ArrayList<>();
 		Trade currentTrade;
 
-		while((currentTrade = createTradeForAuctionMatching(orderBook, openingPrice)) != null) {
+		while ((currentTrade = createTradeForAuctionMatching(orderBook, openingPrice)) != null) {
 			controlResult = auctionMatchingControl.checkBeforeMatch(currentTrade);
 			if (controlResult == ControlResult.OK) {
-				auctionMatchingControl.actionAtMatch(currentTrade, orderBook);
-				trades.add(currentTrade);
+				controllerMatch(currentTrade, orderBook, auctionMatchingControl, trades);
 			} else {
-				auctionMatchingControl.actionAtFailedBeforeMatch(trades, orderBook);
-				return MatchResult.createFromControlResult(controlResult);
+				return controllerFailedBeforeMatch(auctionMatchingControl, trades, orderBook, controlResult);
 			}
 		}
-		
+
 		controlResult = endMatching(auctionMatchingControl, null, orderBook, trades);
 		if (controlResult != ControlResult.OK) {
 			return MatchResult.createFromControlResult(controlResult);
@@ -114,10 +142,15 @@ public class Matcher {
 		return MatchResult.executed(null, trades);
 	}
 
-	private ControlResult endMatching(MatchingControl control, Order targetOrder, OrderBook orderBook, List<Trade> trades) {
+	private ControlResult endMatching(
+		MatchingControl control,
+		Order targetOrder,
+		OrderBook orderBook,
+		List<Trade> trades
+	) {
 		ControlResult result = control.checkAfterMatching(targetOrder, trades);
 		if (result == ControlResult.OK) {
-			control.actionAfterMatching(targetOrder, orderBook);	
+			control.actionAfterMatching(targetOrder, orderBook);
 		} else {
 			control.actionAfterFailedMatching(trades, orderBook);
 		}
@@ -126,12 +159,13 @@ public class Matcher {
 	}
 
 	private Trade createTradeForAuctionMatching(OrderBook orderBook, int openingPrice) {
-		if (!hasOrderForAuction(orderBook)){
+		if (!hasOrderForAuction(orderBook)) {
 			return null;
 		}
 
 		Order sellOrder = orderBook.getHighestPriorityActiveOrder(Side.SELL);
 		Order buyOrder = orderBook.getHighestPriorityActiveOrder(Side.BUY);
+
 		if (sellOrder.canTradeWithPrice(openingPrice) && buyOrder.canTradeWithPrice(openingPrice)) {
 			return new Trade(sellOrder, buyOrder, openingPrice);
 		} else {
@@ -141,28 +175,34 @@ public class Matcher {
 
 	public MatchResult continuousExecuting(Order targetOrder, OrderBook orderBook) {
 		ControlResult controlResult = startingExecution(continuousMatchingControl, targetOrder, orderBook);
+
 		if (controlResult != ControlResult.OK) {
 			return MatchResult.createFromControlResult(controlResult);
 		}
+
 		return continuousMatch(targetOrder, orderBook);
 	}
 
 	public MatchResult auctionExecuting(OrderBook orderBook, int lastTradePrice) {
 		ControlResult controlResult = startingExecution(auctionMatchingControl, null, orderBook);
+		
 		if (controlResult != ControlResult.OK) {
 			return MatchResult.createFromControlResult(controlResult);
 		}
+
 		int openingPrice = calcOpeningAuctionPrice(orderBook, lastTradePrice);
 		return auctionMatch(orderBook, openingPrice);
 	}
 
 	private ControlResult startingExecution(MatchingControl control, Order targetOrder, OrderBook orderBook) {
 		ControlResult controlResult = control.checkBeforeMatching(targetOrder, orderBook);
+
 		if (controlResult == ControlResult.OK) {
 			control.actionAtBeforeMatching(targetOrder, orderBook);
 		} else {
 			control.actionAtFailedBeforeMatching(targetOrder, orderBook);
 		}
+		
 		return controlResult;
 	}
 
@@ -174,10 +214,10 @@ public class Matcher {
 	}
 
 	private Trade createTradeForContinuousMatching(Order targetOrder, Order matchingOrder) {
-        if (targetOrder.isSell()) {
+		if (targetOrder.isSell()) {
 			return new Trade(targetOrder, matchingOrder, matchingOrder.getPrice());
 		} else {
 			return new Trade(matchingOrder, targetOrder, matchingOrder.getPrice());
 		}
-    }
+	}
 }
