e1d1481ce6246d2585f7f143ef2a557e9233298d
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-07-24 07:19:01 +0330

    add new structure for ExpiringService

--- TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/ExpiringService.java
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/ExpiringService.java
@@ -1,6 +1,9 @@
 package ir.ramtung.tinyme.domain.service;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 
 import org.springframework.stereotype.Service;
@@ -9,18 +12,74 @@ import ir.ramtung.tinyme.domain.entity.Order;
 
 @Service
 public class ExpiringService {
+    private static final long checkingRate = 1000;
     private Timer timer;
+    private ScheduleExpiryCheck expiryCheck;
+    private List<Order> todayOrders = new ArrayList<>(); 
+    private List<Order> futureOrders = new ArrayList<>(); 
 
     public ExpiringService() {
         this.timer = new Timer();
+        setup();
     }
 
     public ExpiringService(Timer timer) {
         this.timer = timer;
+        setup();
     }
 
-    public void scheduleexpiryDate(Order order) {
-        long delay = Duration.between(order.getEntryTimes().get(0), order.getExpiryDate()).toSeconds() * 1000;
-		timer.schedule(new ScheduleexpiryDate(order.getSecurity(), order.getSide(), order.getOrderId()), delay);
+    public void addOrder(Order order) {
+        if (todayOrders.contains(order) || futureOrders.contains(order)) {
+            return;
+        }
+
+        if (isOrderForToday(order)) {
+            todayOrders.add(order);
+        }
+        else if (isOrderForFuture(order)) {
+            futureOrders.add(order);
+        }
+    }
+
+    public void updateOrder(Order order) {
+        if (isOrderExpired(order)) {
+            deleteOrder(order);
+        }
+        
+        if (isOrderForToday(order) && futureOrders.contains(order)) {
+            futureOrders.remove(order);
+            todayOrders.add(order);
+        }
+        else if (isOrderForFuture(order) && todayOrders.contains(order)) {
+            todayOrders.remove(order);
+            futureOrders.add(order);
+        }
+    }
+
+    public void deleteOrder(Order order) {
+        //TODO: maybe better way to do this !?
+        if (todayOrders.contains(order)) {
+            todayOrders.remove(order);
+        }
+        if (futureOrders.contains(order)) {
+            futureOrders.remove(order);
+        }
+    }
+
+    private boolean isOrderForToday(Order order) {
+        return LocalDate.now().equals(order.getExpiryDate().toLocalDate());
+    }
+
+    private boolean isOrderForFuture(Order order) {
+        return LocalDate.now().isBefore(order.getExpiryDate().toLocalDate());
+    }
+
+    private boolean isOrderExpired(Order order) {
+        return LocalDate.now().isAfter(order.getExpiryDate().toLocalDate());
+    }
+
+    private void setup() {
+        expiryCheck = new ScheduleExpiryCheck(todayOrders);
+        timer.schedule(expiryCheck, 0, checkingRate);
     }
 }
---
+++ TinyME_v2.0/src/main/java/ir/ramtung/tinyme/domain/service/ScheduleExpiryCheck.java
@@ -0,0 +1,48 @@
+package ir.ramtung.tinyme.domain.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.TimerTask;
+
+import ir.ramtung.tinyme.domain.entity.Order;
+import ir.ramtung.tinyme.domain.entity.Security;
+
+public class ScheduleExpiryCheck extends TimerTask {
+    private final List<Order> todayOrders;
+
+    public ScheduleExpiryCheck(List<Order> todayOrders) {
+        super();
+        this.todayOrders = todayOrders;
+    }
+
+    @Override
+    public void run() {
+        for(Order order : todayOrders) {
+            if (LocalDateTime.now().isBefore(order.getExpiryDate())) {
+                Security security = order.getSecurity();
+                security.deleteOrder(order.getSide(), order.getOrderId());
+            }
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(todayOrders);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }   
+        if (!(other instanceof ScheduleExpiryCheck)) {
+            return false;
+        }
+        ScheduleExpiryCheck expiryCheck = (ScheduleExpiryCheck) other;
+        return expiryCheck.todayOrders.equals(this.todayOrders);
+    }
+}
